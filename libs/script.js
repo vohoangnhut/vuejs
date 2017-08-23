49f426695df350037cd0c20ae1f1e1da450d40d5
Nhut.Vo <nhut.vo@dou.com.vn>
2017-08-23 17:30:35 +0700

    init

--- libs/script.js
+++ libs/script.js
@@ -1,6 +1,36 @@
 
 
-new Vue({
+var app = new Vue({
     el: "#vue-app",
+    data: {
+        lstTask:[
+            {name:'go fishing', iscomplete:true},
+            {name:'play game', iscomplete:true},
+            {name:'fooooooooollllllball time', iscomplete:false},
+            {name:'clean hourse', iscomplete:true},
+            {name:'jelly fish feel', iscomplete:true},
+            {name:'go shopping', iscomplete:true}
+        ],
+        newitem:{name:'', iscomplete:false}
 
+    },
+    methods:{
+        toggleComplete(key){
+            this.lstTask[key].iscomplete = this.lstTask[key].iscomplete ? false:true
+        },
+
+        addNewItem(){
+            this.lstTask.push(this.newitem)
+            this.newitem = {name:'', iscomplete:false}
+        }
+
+    },
+    computed:{
+        incomplatedTask(){
+            return this.lstTask.filter(item => !item.iscomplete)
+        }
+    },
+    mounted(){
+        console.log('document is ready')
+    }
 })
